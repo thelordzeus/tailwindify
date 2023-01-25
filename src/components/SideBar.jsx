b0f4231ef3fc72d13a5da6ff120dd67226700965
thelordzeus <arnav55125@gmail.com>
2023-01-25 19:57:58 +0530

    updated sideBar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,6 +4,7 @@ import { RxCardStack, RxDropdownMenu } from "react-icons/rx";
 import { BiCarousel } from "react-icons/bi";
 import { TfiLayoutAccordionMerged } from "react-icons/tfi";
 import { RiFootprintFill, RiSideBarFill } from "react-icons/ri";
+import { AiOutlineHome } from "react-icons/ai";
 
 export const SideBar = () => {
   return (
@@ -39,6 +40,14 @@ export const SideBar = () => {
           <nav>
             <a
               class="flex items-center px-4 py-2 text-gray-700 bg-gray-100 rounded-md dark:bg-gray-800 dark:text-gray-200"
+              href="#"
+            >
+              <AiOutlineHome className="w-5 h-5" />
+
+              <span class="mx-4 font-medium">Home</span>
+            </a>
+            <a
+              class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
               href="#"
             >
               <TbLayoutNavbar className="w-5 h-5" />
@@ -101,14 +110,18 @@ export const SideBar = () => {
             </a>
           </nav>
 
-          <a href="https://thelordzeus.vercel.app/" target="_blank" class="flex items-center px-4 -mx-2">
+          <a
+            href="https://thelordzeus.vercel.app/"
+            target="_blank"
+            class="flex items-center px-4 -mx-2"
+          >
             <img
               class="object-cover mx-2 rounded-full h-9 w-9"
               src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
               alt="avatar"
             />
             <span class="mx-2 font-medium text-gray-800 dark:text-gray-200">
-              Lord Zeus 
+              Lord Zeus
             </span>
           </a>
         </div>
